d9fe9c5fa877ac7c0efa12f1ca0951ed6782c584
mingstagram <hikariguitar1@gmail.com>
2021-06-02 09:35:41 +0900

    [update] Image DB에 업로드 하기

--- src/main/java/com/cos/photogramstart/web/dto/image/ImageUploadDto.java
+++ src/main/java/com/cos/photogramstart/web/dto/image/ImageUploadDto.java
@@ -2,6 +2,9 @@ package com.cos.photogramstart.web.dto.image;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import com.cos.photogramstart.domain.image.Image;
+import com.cos.photogramstart.domain.user.User;
+
 import lombok.Data;
 
 @Data
@@ -9,5 +12,13 @@ public class ImageUploadDto {
 	
 	private MultipartFile file;
 	private String caption;
+	
+	public Image toEntity(String postImageUrl, User user) {
+		return Image.builder()
+				.caption(caption)
+				.postImageUrl(postImageUrl) // 정확한 경로가 들어가야 됨.
+				.user(user) // 어떤 유저가 사진을 업로드 했는지.
+				.build();
+	}
 	
 }
