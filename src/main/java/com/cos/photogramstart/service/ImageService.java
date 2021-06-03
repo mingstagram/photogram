8eae79561a6e50e247234f2d0c572bead6346473
mingstagram <hikariguitar1@gmail.com>
2021-06-03 16:46:10 +0900

    [update] 스토리페이지 페이징처리 (Pageable 로 구현) - 추후 쌩코딩으로 페이징처리 할 예정

--- src/main/java/com/cos/photogramstart/service/ImageService.java
+++ src/main/java/com/cos/photogramstart/service/ImageService.java
@@ -9,6 +9,8 @@ import java.util.UUID;
 import org.springframework.transaction.annotation.Transactional;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import com.cos.photogramstart.config.auth.PrincipalDetails;
@@ -30,8 +32,8 @@ public class ImageService {
 	
 	
 	@Transactional(readOnly = true) // 영속성 컨텍스트 변경감지를 해서, 더티체킹, flush(반영) X
-	public List<Image> 이미지스토리(int principalId){
-		List<Image> images = imageRepository.mStory(principalId);
+	public Page<Image> 이미지스토리(int principalId, Pageable pageable){
+		Page<Image> images = imageRepository.mStory(principalId, pageable);
 		return images;
 	}
 	
