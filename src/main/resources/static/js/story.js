449b5d429db6a8b998abbbbe92c5693d75515c3c
mingstagram <hikariguitar1@gmail.com>
2021-06-04 11:26:47 +0900

    [update] 댓글쓰기 Ajax 함수 구현

--- src/main/resources/static/js/story.js
+++ src/main/resources/static/js/story.js
@@ -62,9 +62,9 @@ function getStoryItem(image) {
 			<p>${image.caption}</p>
 		</div>
 
-		<div id="storyCommentList-1">
+		<div id="storyCommentList-${image.id}">
 
-			<div class="sl__item__contents__comment" id="storyCommentItem-1"">
+			<div class="sl__item__contents__comment" id="storyCommentItem-1">
 				<p>
 					<b>Lovely :</b> 부럽습니다.
 				</p>
@@ -72,14 +72,13 @@ function getStoryItem(image) {
 				<button>
 					<i class="fas fa-times"></i>
 				</button>
-
 			</div>
 
 		</div>
 
 		<div class="sl__item__input">
-			<input type="text" placeholder="댓글 달기..." id="storyCommentInput-1" />
-			<button type="button" onClick="addComment()">게시</button>
+			<input type="text" placeholder="댓글 달기..." id="storyCommentInput-${image.id}" />
+			<button type="button" onClick="addComment(${image.id})">게시</button>
 		</div>
 
 	</div>
@@ -147,19 +146,36 @@ function toggleLike(imageId) {
 }
 
 // (4) 댓글쓰기
-function addComment() {
+function addComment(imageId) {
 
-	let commentInput = $("#storyCommentInput-1");
-	let commentList = $("#storyCommentList-1");
+	let commentInput = $(`#storyCommentInput-${imageId}`);
+	let commentList = $(`#storyCommentList-${imageId}`);
 
 	let data = {
+		imageId: imageId,
 		content: commentInput.val()
 	}
+	
+	//console.log(data);
+	//console.log(JSON.stringify(data));
 
 	if (data.content === "") {
 		alert("댓글을 작성해주세요!");
 		return;
 	}
+
+	$.ajax({
+		type: "post",
+		url: "/api/comment",
+		data: JSON.stringify(data),
+		contentType: "application/json; charset=utf-8",
+		dataType: "json"
+	}).done(res => {
+		console.log("성공", res);
+	}).fail(error => {
+		console.log("오류", error);
+	});
+	
 
 	let content = `
 			  <div class="sl__item__contents__comment" id="storyCommentItem-2""> 
@@ -170,7 +186,7 @@ function addComment() {
 			    <button><i class="fas fa-times"></i></button>
 			  </div>
 	`;
-	commentList.prepend(content);
+	commentList.prepend(content); // appent는 뒤에다가 추가해주는것 , 최신댓글이 위로올라와야하기에 prepend사용
 	commentInput.val("");
 }
 
